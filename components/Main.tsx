d6eabb6c9d89610ebe6a80d71bee227705db9dc0
Claude De-Tchambila <Claude.De-Tchambila@headoffice.dariel-solutions>
2021-05-05 00:20:55 +0200

    added hero layout

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Main = () => {
-  return <main>Eaglestack</main>;
+  return (
+    <div className="">
+      <section className="mx-44">
+        <div className="grid grid-cols-2 mt-12">
+          <div>
+            <h1>Eaglestack</h1>
+          </div>
+          <div>
+            <h2>Column</h2>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Main;
