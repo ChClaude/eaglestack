24d28602c5e0b2844f880f98ebe6ef46841d39c8
Claude De-Tchambila <christ.tchambila@gmail.com>
2021-06-21 18:02:16 +0200

    updated ecommerce component

--- components/EcommerceSite.tsx
+++ components/EcommerceSite.tsx
@@ -9,8 +9,8 @@ export const EcommerceSite = () => {
           <Image
             src="/assets/icons8-sneakers-64.png"
             alt="Shoe Logo"
-            width={40}
-            height={40}
+            width={50}
+            height={50}
           />
         </div>
         <div className={styles.menu}>
