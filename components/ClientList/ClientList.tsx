330f728a995493cf6b705e6bb47d39884ebc336b
Chris Claude <christ.tchambila@gmail.com>
2021-12-27 00:10:31 +0200

    centered client text

--- components/ClientList/ClientList.tsx
+++ components/ClientList/ClientList.tsx
@@ -8,7 +8,7 @@ const clients: { id: number, name: string, image: string }[] = [
 ];
 
 const ClientList = () => <div className="flex flex-wrap md:grid md:grid-cols-2 lg:grid-cols-4 text-2xl text-gray-700">
-	{clients.map(client => <a href="#" key={client.id} className="mr-4 overflow-hidden last:mr-0 px-8 md:32 lg:44">{client.name}</a>)}
+	{clients.map(client => <a href="#" key={client.id} className="flex justify-center mr-4 overflow-hidden last:mr-0 px-8 md:32 lg:44">{client.name}</a>)}
 </div>;
 
 export default ClientList;
