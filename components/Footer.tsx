c822fc8da11a8db7911a9707af3226b97c5b37a8
Chris Claude <christ.tchambila@gmail.com>
2021-12-26 20:39:47 +0200

    updated footer responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 const Footer = () => (
 	<footer className='bg-blue-50'>
-		<div className='flex flex-col py-12 px-44 md:justify-between md:flex-row'>
+		<div className='flex flex-col py-12 px-8 md:px-32 lg:px-44 md:justify-between md:flex-row'>
 			<div className='flex justify-between'>
-				<ul className='flex text-5xl'>
+				<ul className='w-full flex flex-wrap text-5xl justify-center md:justify-start'>
 					<li className='mr-6'>
 						<a href='#' className='text-gray-600 hover:text-gray-900'>
 							<FontAwesomeIcon
@@ -45,13 +45,13 @@ const Footer = () => (
 				</ul>
 			</div>
 			<div className='mt-4 md:mt-0 md:flex md:items-end'>
-				<ul className='flex'>
-					<li className='md:mr-16'>
+				<ul className='flex flex-wrap justify-center md:justify-start'>
+					<li className='mr-8 md:mr-16'>
 						<Link href='/'>
 							<a className="hover:text-primary">Privacy Policy</a>
 						</Link>
 					</li>
-					<li className='md:mr-16'>
+					<li className='mr-8 md:mr-16'>
 						<Link href='/'>
 							<a className="hover:text-primary">Terms of service</a>
 						</Link>
@@ -66,8 +66,8 @@ const Footer = () => (
 		</div>
 
 		<div
-			className='flex py-7 px-44 flex-col items-center border-t-2 border-solid border-gray-300 md:items-start md:justify-between md:flex-row'>
-			<p className="text-sm"><FontAwesomeIcon
+			className='flex py-7 flex-col items-center border-t-2 border-solid border-gray-300 md:px-44 md:items-start md:justify-between md:flex-row'>
+			<p className="text-sm mb-1 md:mb-0"><FontAwesomeIcon
 				icon='heart'
 				className='text-base text-red-500'
 			/> Made with love in Cape Town.</p>
